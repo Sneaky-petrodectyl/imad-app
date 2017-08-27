cd140d8f2539628fb4ad78238311e5c4e4a0b0f3
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-27 17:13:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,6 +2,9 @@ var button = document.getElementById('counter');
 var counter = 0 ;
 
 button .onclick = function() {
+    //make a request
+    var request = new XMLHttpRequest();
+    
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
