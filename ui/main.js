44928f6312a3a93f963460a2cdaed3cb06c6367c
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-27 12:00:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 alert ('This is a alrt box');
 var element = document.getElementById('main-text');
 element.innerHTML='new value';
-var img = documeny.getElementById('madi');
+var img = document.getElementById('madi');
 img.onclick = function(){
     img.style.marginLeft = '100px';
 };
