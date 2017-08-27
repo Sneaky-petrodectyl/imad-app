d30389f4ef6f34fdfa8ca086a749b30e4800eb3d
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-27 10:19:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,15 +80,15 @@ function createTemplate(data)
         		</div>
         	</body>
         </html>
-        `
+        `;
 }
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName', function (req, res) {
+  res.send(createTemplate(articles(articleName)));
 });
 
 app.get('/article-two', function (req, res) {
