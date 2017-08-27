47bc3eb731d002c0166ea7e707f0c05f17cee61d
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-27 16:30:47 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,8 @@
+var button = getElementById('counter');
+var counter = 0 ;
+
+button .onclick =function(){
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    element.innerHTML = counter.toString();
+}
