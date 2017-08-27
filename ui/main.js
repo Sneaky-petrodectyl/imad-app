aa0b121f33931f614bbf401f7d4d6560efa12d98
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-27 12:10:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,8 +6,11 @@ var element = document.getElementById('main-text');
 element.innerHTML='new value';
 
 var img = document.getElementById('madi');
+
+var marginLeft = 0;
+
 function moveRight(){
-    var marginLeft = 0;
+    
     marginLeft= marginLeft+'10';
     img.style.marginLeft =marginLeft + 'px';
 }
