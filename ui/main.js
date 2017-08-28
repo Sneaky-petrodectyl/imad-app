82c7ced83e99d4ae01132f9673264847d9cb5d19
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-28 13:14:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -25,6 +25,7 @@ var nameInput = document.getElementById('name');
 var name = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
+    var name = nameInput.value;
     var request = new XMLHttpRequest();
     request.onreadystatechange =function(){
         if(request.readyState === XMLHttpRequest.DONE){
