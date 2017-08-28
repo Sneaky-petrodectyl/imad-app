4e367360b008dd778b485018a6eced35988969ca
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-28 11:02:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,10 +12,27 @@ button.onclick = function() {
                 var span = document.getElementById('count');
                 span.innerHTML = counter.toString();
             }
-        
         }
     };
+    
  //make a request
   request.open('GET','http://nileshjha412.imad.hasura-app.io/counter',true);
   request.send(null);
 };
+
+//submit  a  name
+var nameInput = document.geteElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick = function(){
+    //make a reqquest to the server and capture a name
+    //capture a list of names and render it
+    var names = ['name1','name2','name3','name4'];
+    var list = '';
+    for(var i=0;i<names.length;i++){
+        list =='<li>' + names[i] + '<li>'
+    }
+    var ul = document.getElementById('nameslist');
+    ul.innerHTML = list
+    
+}
