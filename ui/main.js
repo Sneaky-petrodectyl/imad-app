239b6dfc74f487a0d1fdea60b19a1f058f7c1854
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-28 11:08:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,7 +27,7 @@ var submit = document.getElementById('submit_btn');
 submit.onclick = function(){
     //make a reqquest to the server and capture a name
     //capture a list of names and render it
-    var names = [`name1`,`name2`,`name3`,`name4`];
+    var names = ['name1','name2','name3','name4'];
     var list = '';
     for(var i=0;i<names.length;i++){
         list =='<li>' + names[i] + '<li>';
