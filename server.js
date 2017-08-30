2ddf15653dd4cb55a89ba23aff65f96d93935bc7
Sneaky-petrodectyl <nileshjha412@gmail.com>
2017-08-30 10:23:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool = require('pg').pool;
 
+var coonfig = {
+    user: 'nileshjha412',
+    database: 'nileshjha412',
+    host : 'db.imad.hasura-app.io/',
+    port : '5432',
+    password : process.env.DB_PASSWORD
+};
 var app = express();
 app.use(morgan('combined'));
 
@@ -86,6 +94,19 @@ function createTemplate(data)
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new pool(config);
+app.get('/test-db', function (req, res) {
+  //make a request
+  //return the response with a result
+  pool.query('SELECT * FROM test', function(err,result){
+      if(err){
+          res.status(500).send(err.toString());
+      }else{
+          res.send(JSON.stringify(result));
+      }
+  });
 });
 
 var names = [];
